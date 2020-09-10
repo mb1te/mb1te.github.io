0ae13b89a30796423e86646be68eb6971639a598
mb1te <45900239+mb1te@users.noreply.github.com>
2020-09-10 20:05:54 +0300

    chm

--- kg/lec_1/script.js
+++ kg/lec_1/script.js
@@ -1,23 +1,23 @@
-canvas = document.getElementById('canvas');
-c = canvas.getContext('2d');
+canvas = document.getElementById('canvas')
+c = canvas.getContext('2d')
 c.fillRect(0, 0, window.innerWidth, window.innerHeight)
 
 c.beginPath()
 
-c.moveTo(388,618);
-c.quadraticCurveTo(100,570,5,175);
-c.quadraticCurveTo(370,295,388,618);
+c.moveTo(388,618)
+c.quadraticCurveTo(100,570,5,175)
+c.quadraticCurveTo(370,295,388,618)
 
 c.moveTo(450, 620)
-c.quadraticCurveTo(265,315,455,5);
-c.quadraticCurveTo(640,315,450,620);
+c.quadraticCurveTo(265,315,455,5)
+c.quadraticCurveTo(640,315,450,620)
 
-c.moveTo(515,618);
-c.quadraticCurveTo(803,570,898,175);
-c.quadraticCurveTo(533,295,515,618);
+c.moveTo(515,618)
+c.quadraticCurveTo(803,570,898,175)
+c.quadraticCurveTo(533,295,515,618)
 
 c.fillStyle = "white"
-c.fill();
+c.fill()
 
 c.fillStyle = "black"
 for (i = 0; i < 3; i++) {
