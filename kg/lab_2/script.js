46cd4e08cbafd8adcd3db0982b0a096008d3f468
mb1te <45900239+mb1te@users.noreply.github.com>
2020-09-29 23:36:22 +0300

    lab2

--- kg/lab_2/script.js
+++ kg/lab_2/script.js
@@ -1,104 +1,95 @@
-ctx = document.querySelector("canvas").getContext("2d")
+'use strict';
 
-var w = 1000
-var h = 700
-var O_x = w / 2
-var O_y = h / 2
-var scale = 50
+var canvas = document.querySelector("canvas")
+var ctx = canvas.getContext("2d")
+
+var ox = canvas.width / 2
+var oy = canvas.height / 2
+var scale = 10
 var rotate = 0
-var x_shift = 0
-var y_shift = 0
+var shift_x = 0
+var shift_y = 0
 
-function draw_labels() {
-    ctx.strokeStyle = "black"
-    ctx.font = '12px Segoe UI';
-    ctx.lineWidth = 1
-    for (var x = 0; x < w; x += 50) {
-        labelX = x * 0.1;
-        if (x != 0) ctx.strokeText((labelX / (scale * 0.1)), w / 2 + 10 + x + x_shift, h / 2 - 5 + y_shift);
-        if (x != 0) ctx.strokeText((-labelX / (scale * 0.1)), w / 2 + 10 - x + x_shift, h / 2 - 5 + y_shift);
-    }
- 
-    for (var y = 0; y < h; y += 50) {
-        labelY = y * 0.1;
-        if (y != 0) ctx.strokeText((-labelY / (scale * 0.1)), w / 2 + 10 + x_shift, h / 2 - 5 + y + y_shift);
-        ctx.strokeText((labelY / (scale * 0.1)), w / 2 + 10 + x_shift, h / 2 - 5 - y + y_shift);
-    }
-}
-
-function axis() {
-    ctx.lineWidth = 4;
-
-    ctx.beginPath();
-    ctx.moveTo(0, h / 2 + y_shift);
-    ctx.lineTo(w, h / 2 + y_shift);
-    ctx.closePath();
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(w / 2 + x_shift, 0);
-    ctx.lineTo(w     / 2 + x_shift, h);
-    ctx.closePath();
-    ctx.stroke();
-}
- 
-//grid X
-function gridOX(x, y) {
-    ctx.beginPath();
-    ctx.strokeStyle = "black";
-    ctx.moveTo(x + x_shift, y);
-    ctx.lineTo(x + 1 + x_shift, y + 1);
-    ctx.closePath();
-    ctx.stroke();
-}
- 
-//grid Y
-function gridOY(y, x) {
-    ctx.beginPath();
-    ctx.strokeStyle = "black";
-    ctx.moveTo(y, x + y_shift);
-    ctx.lineTo(y + 1, x + 1 + transferY);
-    ctx.closePath();
-    ctx.stroke();
-}
-
-function gridFull() {
-    
-    for (var x = w / 2; x < w - x_shift; x += 50)
-        for (var y = 0; y < h; y -= 5)
-            gridOX(x, y);
-    
-    for (var x = w / 2; x < canvas.width - transferX; x += 50)
-        for (var y = canvas.height / 2; y < canvas.height; y += 5)
-        gridOX(x, y);
-    
-    for (var x = canvas.width / 2; x > 0 - transferX; x -= 50)
-        for (var y = canvas.height / 2; y > 0; y -= 5)
-        gridOX(x, y);
-    
-    for (var x = canvas.width / 2; x > 0 - transferX; x -= 50)
-        for (var y = canvas.height / 2; y < canvas.height; y += 5)
-        gridOX(x, y);
- 
+function draw_numbers() {
     //OX
-    // right-top
-    for (var x = canvas.height / 2; x < canvas.height - transferY; x += 50)
-        for (var y = canvas.width / 2; y > 0; y -= 5)
-            gridOY(y, x, transferX, transferY);
-    // right-bottom
-    for (var x = canvas.height / 2; x < canvas.height - transferY; x += 50)
-        for (var y = canvas.width / 2; y < canvas.width; y += 5)
-            gridOY(y, x, transferX, transferY);
-    // left-top
-    for (var x = canvas.height / 2; x > 0 - transferY; x -= 50)
-        for (var y = canvas.width / 2; y > 0; y -= 5)
-            gridOY(y, x, transferX, transferY);
-    // left-bottom
-    for (var x = canvas.height / 2; x > 0 - transferY; x -= 50)
-        for (var y = canvas.width / 2; y < canvas.width; y += 5)
-            gridOY(y, x, transferX, transferY);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+        ctx.font = '12px Segoe UI';
+    for (var x = 0; x < canvas.width; x += 50) {
+        var x_number = x * 0.1;
+        ctx.strokeText((x_number / (scale * 0.1)), canvas.width / 2 + 10 + x + shift_x, canvas.height / 2 - 5 + shift_y);
+        ctx.strokeText((-x_number / (scale * 0.1)), canvas.width / 2 + 10 - x + shift_x, canvas.height / 2 - 5 + shift_y);
+    }
+ 		//OY
+    for (var y = 0; y < canvas.height; y += 50) {
+        var y_number = y * 0.1;
+        ctx.strokeText((-y_number / (scale * 0.1)), canvas.width / 2 + 10 + shift_x, canvas.height / 2 - 5 + y + shift_y);
+        ctx.strokeText((y_number / (scale * 0.1)), canvas.width / 2 + 10 + shift_x, canvas.height / 2 - 5 - y + shift_y);
+    }
 }
 
-draw_labels()
-axis()  
-gridFull()
+function draw_lines() {
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 3;
+    //OX
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height / 2 + shift_y);
+    ctx.lineTo(canvas.width, canvas.height / 2 + shift_y);
+    ctx.closePath();
+    ctx.stroke();
+    //OY
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2 + shift_x, 0);
+    ctx.lineTo(canvas.width / 2 + shift_x, canvas.height);
+    ctx.closePath();
+    ctx.stroke();
+}
+
+function draw_ver(x, y) {
+    ctx.beginPath();
+    ctx.moveTo(x + shift_x, y);
+    ctx.lineTo(x + 3 + shift_x, y);
+    ctx.closePath();
+    ctx.stroke();
+}
+ 
+
+function draw_hor(x, y) {
+    ctx.beginPath()
+    ctx.moveTo(x, y + shift_y)
+    ctx.lineTo(x, y + 3 + shift_y)
+    ctx.closePath()
+    ctx.stroke()
+}
+ 
+//grid full
+function draw_grid() {
+    ctx.strokeStyle = "#6B90D4"
+    for (var x = 0; x < canvas.width - shift_x; x += 50) {
+        for (var y = canvas.height; y > 0; y -= 10) {
+            draw_hor(x, y);
+        }
+    }
+    for (var y = 0; y < canvas.height - shift_y; y += 50) {
+        for (var x = canvas.width; x > 0; x -= 10) {
+            draw_ver(x, y);
+        }
+    }
+}
+
+function draw_plot() {
+    ctx.beginPath();
+    ctx.moveTo(ox, oy);
+    for (var t = 0; t <= 2 * Math.PI; t += 0.01) {
+        var x = ox + 2 * Math.cos(t) + Math.cos(2 * t);
+        var y = oy + 2 * Math.sin(t) - Math.sin(2 * t);
+        ctx.lineTo(5*x, 5*y);
+    }
+    ctx.strokeStyle = 'red';
+    ctx.stroke();    
+}
+
+draw_grid();
+draw_numbers();
+draw_lines();
+draw_plot();
